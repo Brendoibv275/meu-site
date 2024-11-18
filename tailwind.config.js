fae4c65dc47785a1ed1943fdcc549c008667d9fe
Brendoibv275 <adbbrendo@gmail.com>
2024-11-18 19:18:12 -0300

    Ajuste no Botao do chat
    Mudanca para nao abrir automatico e reduzir o impacto visual dele

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./public/**/*.{html,js}"],
+    safelist: [
+      'scale-0',
+      'scale-100',
+      'opacity-0',
+      'opacity-100',
+      'rotate-45',
+      'hidden',
+      'animate-bounce'
+    ],
     theme: {
       extend: {
         colors: {
